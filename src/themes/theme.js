5d920a7f73585b2c0ac5a43be934ecdc0d353e07
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-09-09 23:33:02 +0200

    Create theme.js
    Theme created

---
+++ src/themes/theme.js
@@ -0,0 +1,28 @@
+export default {
+  color: {
+    primary: "#40A829",
+    secondary: "#006241",
+    white: "#FFFFFF",
+    text: "#DDDDDD",
+    background: "#333333",
+  },
+  font: {
+    xl: "4rem",
+    l: "3.5rem",
+    m: "2.2rem",
+    s: "2.1rem",
+    xs: "1.7rem",
+    xxs: "1.6rem",
+    black: 900,
+    bold: 700,
+    medium: 400,
+    thin: 300,
+    name: "'Lato', sans-serif",
+  },
+  breakpoints: {
+    desktop: "1600px",
+    smallDesktop: "1200px",
+    tablet: "768px",
+    mobile: "480px",
+  },
+}
