2a5a3f730496ce2fdf5dcb91d106bc396fb10101
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-09-24 19:22:30 +0200

    main created and added props with childrens

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,12 +4,15 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyle from "../themes/GlobalStyle"
 import routes from "./paths"
 import Navigation from "../components/Navigation/Navigation"
+import Header from "../components/Header/Header"
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Navigation routes={routes} />
+      <Header />
+      <main>{children}</main>
     </ThemeProvider>
   )
 }
