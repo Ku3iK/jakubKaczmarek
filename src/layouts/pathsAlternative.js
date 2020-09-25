cfc498dfaa4f0bcc6303c4a0c1bcdd507b574d6d
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-09-25 17:38:01 +0200

    Create pathsAlternative.js

---
+++ src/layouts/pathsAlternative.js
@@ -0,0 +1,18 @@
+import GitHubIcon from "../assets/icons/github-black.svg"
+import FacebookIcon from "../assets/icons/facebook-black.svg"
+import LinkedinIcon from "../assets/icons/linkedin-black.svg"
+
+export default [
+  {
+    icon: GitHubIcon,
+    path: "https://github.com/Ku3iK",
+  },
+  {
+    icon: FacebookIcon,
+    path: "https://www.facebook.com/kubakaczmarek02/",
+  },
+  {
+    icon: LinkedinIcon,
+    path: "https://www.linkedin.com/in/jakub-kaczmarek-3370531b3/",
+  },
+]
